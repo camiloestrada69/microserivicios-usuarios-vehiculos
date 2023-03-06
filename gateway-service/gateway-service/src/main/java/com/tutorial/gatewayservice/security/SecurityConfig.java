c141d3f42b545c556473bb3e6fe8032a6be64776
Camilo Estrada <camilo.estrada@bpt.com.co>
2023-03-06 11:42:15 -0500

    keycloak

--- gateway-service/gateway-service/src/main/java/com/tutorial/gatewayservice/security/SecurityConfig.java
+++ gateway-service/gateway-service/src/main/java/com/tutorial/gatewayservice/security/SecurityConfig.java
@@ -3,13 +3,12 @@ package com.tutorial.gatewayservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
